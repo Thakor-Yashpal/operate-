bb1f4b7c22bf40dacc6233c06bd119540772f3a4
yashpal <75574310+nova7665@users.noreply.github.com>
2022-09-15 01:32:19 +0530

    Create commi.jl

---
+++ commi.jl
@@ -0,0 +1,42 @@
+//operate 
+
+console.log("operators in js")
+
+let a = 45;
+let b = 4;
+
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a / b =", a/b);
+console.log("a ** b =", a**b);
+console.log("a % b =", a%b);
+
+//11
+
+console.log("a++b =", a++);
+console.log("a-- =", a--);
+
+// assignment operators
+let ba= 2;
+ba += 5 
+conole.log(ba)
+//comparison operators 
+
+let comp1 =6;
+let comp2 ="7";
+
+consolelog("comp1 == comp2 is", comp1 == comp2)
+consolelog("comp1 != comp2 is", comp1 != comp2)
+consolelog("comp1 === comp2 is", comp1 === comp2)
+consolelog("comp1 !== comp2 is", comp1 !== comp2)
+consolelog("comp1 > comp2 is", comp1 > comp2)
+
+//logical operators
+
+let x = 5;
+let y = 6;
+console.log(x<y && x==5) 
+console.log(x > y || x==5) 
+console.log(!false) 
+console.log(!true)
+
